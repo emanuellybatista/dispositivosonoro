b7ca3728c0983dbc64dfb121ba3b733491542ed7
emanuellybatista <105320068+emanuellybatista@users.noreply.github.com>
2023-10-27 10:27:42 -0300

    wile

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ function tocaSomPom(){
 
 
 //criando areferências constante listaDeTeclas e buscando a tecla
-const listaDeTeclas=document.querySelectorAll("tecla");
-//chamando a constante lista De teclas e o indice "0" do SomPom
-listaDeTeclas[0] .onclick = tocaSomPom;                                                                                                                
+const listaDeTeclas = document.querySelectorAll('.tecla');
+                                      
+
+let contador = 0;
+while(contador < 9){
+    listaDeTeclas[contador] .onclick = tocaSomPom; 
+    contador = contador + 1;
+    console.log(contador);
+}
