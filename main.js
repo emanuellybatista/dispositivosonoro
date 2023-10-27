911fa78b6157a006415fb1fd8e7ee26dfa77b1c6
emanuellybatista <105320068+emanuellybatista@users.noreply.github.com>
2023-10-27 10:10:51 -0300

    ok

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-document.querySelector('olá');
+//declaracao.criacao da funcao tocaSomPom
+function tocaSomPom(){
+    document.querySelector("som_tecla_pom").play();
+}
+
+
+//criando areferências constante listaDeTeclas e buscando a tecla
+const listaDeTeclas=document.querySelectorAll("tecla");
+//chamando a constante lista De teclas e o indice "0" do SomPom
+listaDeTeclas[0] .onclick = tocaSomPom
