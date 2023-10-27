9ec1f728b878b3b36249532130f4dcb02b4399f1
emanuellybatista <105320068+emanuellybatista@users.noreply.github.com>
2023-10-27 10:30:59 -0300

    jkm

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 while(contador < 9){
-    listaDeTeclas[contador] .onclick = tocaSomPom; 
+    listaDeTeclas[contador].onclick = tocaSomPom; 
     contador = contador + 1;
     console.log(contador);
 }
